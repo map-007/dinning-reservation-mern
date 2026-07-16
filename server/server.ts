05850b70bffe5566249ba48c61209ccdd499072c
map-007 <min.aungpaing007@gmail.com>
2026-07-17 02:04:53 +0630

    Create api for auth

---
+++ server/server.ts
@@ -0,0 +1,33 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
+
+const app = express();
+
+// Middleware
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is Live!");
+});
+app.use("/api/auth", authRouter);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error("Unhandle Error", err);
+  res.status(500).json({
+    message: err.message || "Internal Server Error",
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
